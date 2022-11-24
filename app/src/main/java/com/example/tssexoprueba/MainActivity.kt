0da1a6922506da2df666248189835bb54197a34e
ricardomorarey <ricardomorarey@gmail.com>
2022-11-24 10:50:25 +0100

    refactor separate class the player

--- app/src/main/java/com/example/tssexoprueba/MainActivity.kt
+++ app/src/main/java/com/example/tssexoprueba/MainActivity.kt
@@ -23,84 +23,31 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val playbackStateListener: Player.Listener = playbackStateListener()
-    private var player: ExoPlayer? = null
-
-    private var playWhenReady = true
-    private var currentItem = 0
-    private var playbackPosition = 0L
+    val mymedia1: Media3Streaming? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-        //initializePlayer()
 
+        val mymedia1 = Media3Streaming(this, viewBinding)
         viewBinding.button.setOnClickListener {
-            initializePlayer()
+            mymedia1.initializePlayer()
         }
     }
 
-    public override fun onStart() {
-        super.onStart()
-        if (Util.SDK_INT > 23) {
-            //initializePlayer()
-        }
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        hideSystemUi()
-        if (Util.SDK_INT <= 23 || player == null) {
-            //initializePlayer()
-        }
-    }
 
     public override fun onPause() {
         super.onPause()
-        if (Util.SDK_INT <= 23) {
-            releasePlayer()
+        if (mymedia1 != null) {
+            mymedia1.releasePlayer()
         }
     }
 
     public override fun onStop() {
         super.onStop()
-        if (Util.SDK_INT > 23) {
-            releasePlayer()
+        if (mymedia1 != null) {
+            mymedia1.releasePlayer()
         }
-    }
-
-
-    private fun initializePlayer() {
-        val trackSelector = DefaultTrackSelector(this).apply {
-            setParameters(buildUponParameters().setMaxVideoSizeSd())
-        }
-        player = ExoPlayer.Builder(this)
-            .setTrackSelector(trackSelector)
-            .build()
-            .also { exoPlayer ->
-                viewBinding.videoView.player = exoPlayer
-
-                val mediaItem = MediaItem.Builder()
-                    .setUri("rtsp://admin:Demes2323@hik1pro2.davidsat.dnsdemes.com:554/ISAPI/Streaming/Channels/301")
-                    .setMimeType(MimeTypes.APPLICATION_RTSP)
-                    .build()
-                exoPlayer.setMediaItem(mediaItem)
-                exoPlayer.playWhenReady = playWhenReady
-                exoPlayer.seekTo(currentItem, playbackPosition)
-                exoPlayer.addListener(playbackStateListener)
-                exoPlayer.prepare()
-            }
-    }
-
-    private fun releasePlayer() {
-        player?.let { exoPlayer ->
-            playbackPosition = exoPlayer.currentPosition
-            currentItem = exoPlayer.currentMediaItemIndex
-            playWhenReady = exoPlayer.playWhenReady
-            exoPlayer.removeListener(playbackStateListener)
-            exoPlayer.release()
-        }
-        player = null
     }
 
     @SuppressLint("InlinedApi")
@@ -114,15 +61,3 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-private fun playbackStateListener() = object : Player.Listener {
-    override fun onPlaybackStateChanged(playbackState: Int) {
-        val stateString: String = when (playbackState) {
-            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
-            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
-            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
-            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
-            else -> "UNKNOWN_STATE             -"
-        }
-        Log.d(TAG, "changed state to $stateString")
-    }
-}
---
+++ app/src/main/java/com/example/tssexoprueba/Media3Streaming.kt
@@ -0,0 +1,71 @@
+package com.example.tssexoprueba
+
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import android.view.View
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import com.example.tssexoprueba.databinding.ActivityMainBinding
+
+class Media3Streaming(context: Context, binding: ActivityMainBinding) {
+
+    private var player: ExoPlayer? = null
+    var viewBinding = binding
+    var context = context
+
+    private val playbackStateListener: Player.Listener = playbackStateListener()
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+
+    fun initializePlayer() {
+        val trackSelector = DefaultTrackSelector(context).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+        player = ExoPlayer.Builder(context)
+            .setTrackSelector(trackSelector)
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+
+                val mediaItem = MediaItem.Builder()
+                    .setUri("rtsp://")
+                    .setMimeType(MimeTypes.APPLICATION_RTSP)
+                    .build()
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.addListener(playbackStateListener)
+                exoPlayer.prepare()
+            }
+    }
+
+    fun releasePlayer() {
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.removeListener(playbackStateListener)
+            exoPlayer.release()
+        }
+        player = null
+    }
+}
+
+fun playbackStateListener() = object : Player.Listener {
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val stateString: String = when (playbackState) {
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+            else -> "UNKNOWN_STATE             -"
+        }
+        Log.d(ContentValues.TAG, "changed state to $stateString")
+    }
+}
